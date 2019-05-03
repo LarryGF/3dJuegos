00f522782dec31234ec61f45b3aca106798f0ae2
LarryGF <larrywtf609@gmail.com>
2019-05-03 14:58:58 -0400

    Saving each game to an individual .json, and each thumbnail

--- getgames.py
+++ getgames.py
@@ -1,10 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import os
+import urllib3
 from fire import Fire
 
 base_url = 'https://www.3djuegos.com/novedades/juegos-generos/juegos/{}pf0f0f0/juegos-populares/'
+if not os.path.exists('games'):
+     os.makedirs('games')
 
+games_dir = os.path.join(os.getcwd(), 'games')
 
 def get_game(url):
     r = requests.get(url)
@@ -38,11 +43,28 @@ def get_games_list(url):
 def get_all_games():
 
     ans = []
-    for i in range(112):
-        ans.extend([get_game(url)
-                    for url in get_games_list(base_url.format(i))])
+    for i in range(612):
+        for url in get_games_list(base_url.format(i)):
+            game = get_game(url)
+            game['name'] = game['name'].replace('/','-')
+            print(game['name'])
+            if not os.path.exists(os.path.join(games_dir, game['name'] + '.json')):
+                print(game['name']+ ' saving json')
+                json.dump(game, open(os.path.join(games_dir, game['name'] + '.json'), 'w'))
+                thumbnail = requests.get(game['thumbnailUrl'])
+                if not os.path.exists(os.path.join(games_dir, game['name'] + '_th.jpg')):
+                    print(game['name']+ 'saving jpg')
+                    file = open(os.path.join(games_dir, game['name'] + '_th.jpg'), 'wb')
+                    file.write(thumbnail.content)
+                    file.close()
+        # ans.extend([get_game(url)
+        #             for url in get_games_list(base_url.format(i))])
+        # print(ans)
 
-    return ans
+    
+    # json.dump(ans, open('games.json', 'w'))
+
+    # return ans
 
 
 if __name__ == "__main__":
