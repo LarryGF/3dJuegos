4fab1e82117bb9c08ec9e000390c6728243113cb
hian <hiancdtrsnm@gmail.com>
2019-05-06 00:14:05 -0400

    Complete standard

--- getgames.py
+++ getgames.py
@@ -4,15 +4,39 @@ import json
 import os
 import urllib3
 from fire import Fire
+import logging
+from path import Path
+import enlighten
+from slugify import slugify
+
+logger = logging.getLogger('3djuegos')
+logging.basicConfig(level=logging.DEBUG)
+
+logger.info('This is running SYNC and WITHOUT POOL')
 
 base_number = 0
 base_url = 'https://www.3djuegos.com/novedades/juegos-generos/juegos/{}pf0f0f0/juegos-populares/'
+
+
+class Config():
+    thumbnail = False
+    image = False
+    overwrite = False
+
+
+saved_games = []
+
 if not os.path.exists('games'):
-     os.makedirs('games')
+    os.makedirs('games')
+
+base_path = Path('games')
 
 games_dir = os.path.join(os.getcwd(), 'games')
 
-def get_game(url):
+
+def get_game_data(url):
+
+    logger.debug(f'Download game info from: {url}')
     r = requests.get(url)
 
     assert r.status_code == 200, "The request was not sucesfull"
@@ -25,11 +49,29 @@ def get_game(url):
 
     data = json.loads(data.text)
     data['description'] = description
+    data['safe-name'] = slugify(data['name'])
 
     return data
 
 
+def get_thumbail(data):
+    logger.debug(
+        f'Download thumbail of {data["name"]} from: {data["thumbnailUrl"]}')
+
+    thumbnail = requests.get(data['thumbnailUrl'])
+    (base_path/data['safe-name'] + '-thumb.jpg').write_bytes(thumbnail.content)
+
+
+def get_image(data):
+    logger.debug(f'Download image of {data["name"]} from: {data["image"]}')
+
+    thumbnail = requests.get(data['image'])
+    (base_path/data['safe-name'] + '.jpg').write_bytes(thumbnail.content)
+
+
 def get_games_list(url):
+    logger.debug(f'Download game list from: {url}')
+
     r = requests.get(url)
 
     assert r.status_code == 200, "The request was not sucesfull"
@@ -41,35 +83,50 @@ def get_games_list(url):
     return [a.a.attrs['href'] for a in articles]
 
 
-def get_all_games():
+def get_game(url):
 
-    if os.path.exists('last.json'):
-        base_number = json.load(open('last.json'))['number']
-    
-    try:
-        for i in range(base_number,612):
-            
-            for url in get_games_list(base_url.format(i)):
-                game = get_game(url)
-                if type(game['name']) == str:
-                    game['name'] = game['name'].replace('/','-')
-                print(game['name'])
-                if not os.path.exists(os.path.join(games_dir, game['name'] + '.json')):
-                    print(game['name']+ ' saving json')
-                    json.dump(game, open(os.path.join(games_dir, game['name'] + '.json'), 'w'))
-                    # thumbnail = requests.get(game['thumbnailUrl'])
-                    # if not os.path.exists(os.path.join(games_dir, game['name'] + '_th.jpg')):
-                    #     print(game['name']+ 'saving jpg')
-                    #     file = open(os.path.join(games_dir, game['name'] + '_th.jpg'), 'wb')
-                    #     file.write(thumbnail.content)
-                    #     file.close()
-    except:
-        print('saving last page')
-        print(i)
-        json.dump({'number':i},open('last.json','w'))
-       
+    if url in saved_games and not Config.overwrite:
+        return
+
+    data = get_game_data(url)
+
+    json.dump(data, (base_path/data['safe-name'] + '.json').open('w'),
+              ensure_ascii=False, indent=2)
+
+    if Config.thumbnail:
+        get_thumbail(data)
+
+    if Config.image:
+        get_image(data)
+
+
+def get_all_games(begining=0, end=611):
+    logger.debug(f'From {begining} to {end}')
+    manager = enlighten.get_manager()
+
+    pages = manager.counter(total=end-begining, desc='Page:', unit='pages')
+
+    for i in range(begining, end):
+
+        game_list = get_games_list(base_url.format(i))
+
+        games = manager.counter(total=len(game_list),
+                                desc='  Game:', unit='games', leave=False)
+
+        for url in get_games_list(base_url.format(i)):
+
+            get_game(url)
+
+            # except Exception as e:
+            #     logger.error(e)
+
+            games.update()
+        games.close()
+        pages.update()
+
+    manager.stop()  # Clears all temporary counters and progress bars
 
 
 if __name__ == "__main__":
-    # Fire()
-    get_all_games()
+    Fire()
+    # get_all_games()
